1d71dc02be068cd996ca35bfe116a6b7dc5ed769
Andrew <andrewdruzhaev@gmail.com>
2021-04-21 17:28:46 +0300

    Implemented files reading for mock data

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,4 +1,6 @@
 const chalk = require(`chalk`);
+const fs = require(`fs/promises`);
+const path = require(`path`);
 
 const {
   getRandomInt,
@@ -6,44 +8,11 @@ const {
   getPictureFileName,
 } = require(`../../utils`);
 
-const fs = require(`fs/promises`);
-
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга.`,
-  `Продам новую приставку Sony Playstation 5.`,
-  `Продам отличную подборку фильмов на VHS.`,
-  `Куплю антиквариат.`,
-  `Куплю породистого кота.`,
-  `Продам коллекцию журналов «Огонёк».`,
-  `Отдам в хорошие руки подшивку «Мурзилка».`,
-  `Продам советскую посуду. Почти не разбита.`,
-  `Куплю детские санки.`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
+const FILE_SENTENCES_PATH = path.join(__dirname, `../../data/sentences.txt`);
+const FILE_CATEGORIES_PATH = path.join(__dirname, `../../data/categories.txt`);
+const FILE_TITLES_PATH = path.join(__dirname, `../../data/titles.txt`);
 
 const OfferType = {
   OFFER: `offer`,
@@ -55,23 +24,37 @@ const PriceRange = {
   MAX: 10000,
 };
 
-const generateOffers = (count) => (
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    category: [shuffle(CATEGORIES).slice(1, getRandomInt(0, CATEGORIES.length - 1)).join(`, `)],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(0, 16), getRandomInt),
     sum: getRandomInt(PriceRange.MIN, PriceRange.MAX),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: OfferType[Object.keys(OfferType)[getRandomInt(0, Object.keys(OfferType).length - 1)]]
   }))
 );
 
+const readContent = async (filePath) => {
+  try {
+    console.log(`Current directory: ${process.cwd()}`);
+    const content = await fs.readFile(filePath, `utf-8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(`File can't be read, ${err}`));
+    return [];
+  }
+};
+
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
