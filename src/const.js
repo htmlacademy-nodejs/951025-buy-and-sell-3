fe6530f03055bace339ca0ccbe6279327032e9c4
Andrew <andrewdruzhaev@gmail.com>
2021-04-24 18:02:04 +0300

    Added new CLI command that runs http server

--- src/const.js
+++ src/const.js
@@ -1,13 +1,24 @@
 const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 2;
+const DEFAULT_PORT = 3000;
 
 const ExitCode = {
   SUCCESS: 0,
   FAIL: 1,
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ExitCode,
+  HttpCode,
+  DEFAULT_PORT
 };
