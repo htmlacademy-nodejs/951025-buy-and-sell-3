a8cf7b9fefc9b000c9ffeac5017f28e27cc40a62
Andrew <andrewdruzhaev@gmail.com>
2021-07-15 08:04:07 +0300

    Create CommentService

---
+++ src/service/data-service/comment.js
@@ -0,0 +1,35 @@
+const {nanoid} = require(`nanoid`);
+const {MAX_ID_LENGTH} = require(`../../constants`);
+
+class CommentService {
+  create(offer, comment) {
+    const newComment = Object.assign({
+      id: nanoid(MAX_ID_LENGTH),
+      comment,
+    });
+
+    offer.comments.push(newComment);
+
+    return newComment;
+  }
+
+  drop(offer, commentId) {
+    const dropComment = offer.comments
+      .find((comment) => comment.id === commentId);
+
+    if (!dropComment) {
+      return null;
+    }
+
+    offer.comments = offer.comments
+      .filter((comment) => comment.id !== commentId);
+
+    return dropComment;
+  }
+
+  findAll(offer) {
+    return offer.comments;
+  }
+}
+
+module.exports = CommentService;
