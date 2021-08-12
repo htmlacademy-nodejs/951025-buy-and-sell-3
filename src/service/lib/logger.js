24d1d3324be3bd2d530c25c934b6934d5433fc4c
Andrew <andrewdruzhaev@gmail.com>
2021-08-12 15:19:04 +0300

    Add pino package & create logger

---
+++ src/service/lib/logger.js
@@ -0,0 +1,13 @@
+const pino = require(`pino`);
+
+const logger = pino({
+  name: `base-logger`,
+  level: `info`,
+});
+
+module.exports = {
+  logger,
+  getLogger(options = {}) {
+    return logger.child(options);
+  }
+};
