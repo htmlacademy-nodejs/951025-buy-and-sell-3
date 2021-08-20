c1b41a1bb83aa76ee1df80a87b61cc99bb639b2b
Andrew <andrewdruzhaev@gmail.com>
2021-08-20 19:17:09 +0300

    Refactor search tests

--- src/service/api/search.e2e.test.js
+++ src/service/api/search.e2e.test.js
@@ -1,42 +1,66 @@
 const express = require(`express`);
 const request = require(`supertest`);
 const search = require(`./search`);
-const DataService = require(`../data-service/search`);
+const SearchService = require(`../data-service/search`);
 const mockData = require(`../../data/mockData`);
 const {HttpCode} = require(`../../const`);
 
-const app = express();
-app.use(express.json());
-search(app, new DataService(mockData));
+const createApi = (services = []) => {
+  const app = express();
+  app.use(express.json());
+  search(app, ...services);
+  return app;
+};
+describe(`Search`, () => {
+  describe(`API returns offer based on query`, () => {
+    let app = null;
+    let response = null;
+    let searchService = null;
 
-describe(`API returns offer based on search query`, () => {
+    beforeEach(() => {
+      searchService = new SearchService(mockData);
+      app = createApi([searchService]);
+    });
 
-  let response;
-
-  beforeAll(async () => {
-    response = await request(app)
-      .get(`/search`)
-      .query({
+    test(`Status code 200`, async () => {
+      response = await request(app).get(`/search`).query({
         query: `Продам книги Стивена Кинга`
       });
+
+      expect(response.statusCode).toBe(HttpCode.OK);
+    });
+    test(`Returns one offer`, async () => {
+      response = await request(app).get(`/search`).query({
+        query: `Продам книги Стивена Кинга`
+      });
+
+      expect(response.body.length).toBe(1);
+    });
+    test(`Offer's id equals "j8gydnqp"`, async () => {
+      response = await request(app).get(`/search`).query({
+        query: `Продам книги Стивена Кинга`
+      });
+
+      expect(response.body[0].id).toBe(`j8gydnqp`);
+    });
   });
 
-  test(`Status code 200`, () => expect(response.statusCode).toBe(HttpCode.OK));
-  test(`1 offer found`, () => expect(response.body.length).toBe(1));
-  test(`Offer has correct id`, () => expect(response.body[0].id).toBe(`j8gydnqp`));
-});
+  describe(`API refuses to return offer`, () => {
+    let app = null;
+    let searchService = null;
 
-test(`API returns 404 code if nothing is found`, () => {
-  request(app)
-    .get(`/search`)
-    .query({
-      query: `Куплю породистого кота`
-    })
-    .expect(HttpCode.NOT_FOUND);
-});
+    beforeEach(() => {
+      searchService = new SearchService(mockData);
+      app = createApi([searchService]);
+    });
+    test(`Status code 404 if nothing is found`, (done) => {
+      request(app).get(`/search`).query({
+        query: `Нет такого заголовка`
+      }).expect(HttpCode.NOT_FOUND, done);
+    });
 
-test(`API returns 400 when query string is absent`, () => {
-  request(app)
-    .get(`/search`)
-    .expect(HttpCode.BAD_REQUEST);
+    test(`Status code 400 when query string is absent`, (done) => {
+      request(app).get(`/search`).expect(HttpCode.BAD_REQUEST, done);
+    });
+  });
 });
