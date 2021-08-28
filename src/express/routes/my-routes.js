c59763d8ff23c0c2db84110338c80a38281232f8
Andrew <andrewdruzhaev@gmail.com>
2021-08-28 12:42:35 +0300

    Implement my offers query & page

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,7 +1,16 @@
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my-tickets`));
+myRouter.get(`/`, async (req, res) => {
+  try {
+    const offers = await api.getOffers();
+    res.render(`my-tickets`, {offers});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRouter;
