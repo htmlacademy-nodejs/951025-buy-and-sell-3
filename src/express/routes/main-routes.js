7d41a477cc806b0b07f23d62b079586b14519937
Andrew <andrewdruzhaev@gmail.com>
2021-08-28 11:37:50 +0300

    Use API to get offers & update main.pug

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,7 +1,16 @@
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  try {
+    const offers = await api.getOffers();
+    res.render(`main`, {offers});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
