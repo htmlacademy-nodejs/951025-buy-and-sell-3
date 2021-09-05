a2621e51797b9bad6ba97459d71d1b19e1397fa4
Andrew <andrewdruzhaev@gmail.com>
2021-09-05 13:04:06 +0300

    Update offers-routes

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,9 +1,73 @@
 const {Router} = require(`express`);
+const multer = require(`multer`);
+const path = require(`path`);
+const {nanoid} = require(`nanoid`);
+const api = require(`../api`).getAPI();
+const {ensureArray} = require(`../../utils`);
 const offersRouter = new Router();
 
+const UPLOAD_DIR = `../upload/img/`;
+
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
+
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+  filename: (req, file, callback) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    callback(null, `${uniqueName}.${extension}`);
+  }
+});
+
+const upload = multer({storage});
+
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
-offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
+
+offersRouter.get(`/add`, async (req, res) => {
+  try {
+    const categories = await api.getCategories();
+    res.render(`new-ticket`, {categories});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+  const {body, file} = req;
+
+  const offerData = {
+    picture: file ? file.filename : ``,
+    sum: body.price,
+    type: body.action,
+    description: body.comment,
+    title: body[`ticket-name`],
+    categories: ensureArray(body.category)
+  };
+
+  try {
+    await api.createOffer(offerData);
+    res.redirect(`/my`);
+  } catch (error) {
+    console.log(error);
+    res.redirect(`back`);
+  }
+});
+
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const [offer, categories] = await Promise.all([
+      api.getOffer(id),
+      api.getCategories()
+    ]);
+
+    res.render(`ticket-edit`, {offer, categories});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
 
 module.exports = offersRouter;
